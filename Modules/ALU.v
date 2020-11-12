759cfee2ac905731f8d9373c01f76939ec37b3bc
V Dheeraj <41289602+dheerajrox@users.noreply.github.com>
2020-11-12 10:52:42 +0530

    Adding ALU, ALU Control, Control Unit

---
+++ Modules/ALU.v
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+
+module ALU(clk, x, y, out, ALUFn, zero, ng, overflow);
+	input wire clk;
+	input wire [31:0] x;
+	input wire [31:0] y;
+	input wire [4:0] ALUFn;
+	output reg [31:0] out;
+	output reg zero; 
+	output reg ng; 
+	output reg overflow;
+
+	always @(x or y or ALUFn)
+	begin
+		if (ALUFn[4:0] == 4'b0010) begin // ADD
+			out = x + y;
+			zero = (out==0)?1:0;
+			ng = (out<0)?1:0;
+			if ((x >= 0 && y >= 0 && out < 0) || (x < 0 && y < 0 && out >= 0)) 
+                    		overflow = 1;
+                	else
+                    		overflow = 0;
+                	
+            	end else if (ALUFn[4:0] == 4'b0110) begin // SUB
+			out = x - y;
+			zero = (out==0)?1:0;
+			ng = (out<0)?1:0;
+			if ((x >= 0 && y >= 0 && out < 0) || (x < 0 && y < 0 && out >= 0)) 
+                    		overflow = 1;
+                	else
+                    		overflow = 0;
+			
+		end else if (ALUFn[4:0] == 4'b0000) begin // AND
+			out = x & y;
+			
+			zero = (out==0)?1:0;
+			ng = (out<0)?1:0;
+			overflow = 0;
+		end else if (ALUFn[4:0] == 4'b0001) begin // OR
+			out = x | y;
+			
+			zero = (out==0)?1:0;
+			ng = (out<0)?1:0;
+			overflow = 0;
+		end else if (ALUFn[4:0] == 4'b0011) begin // XOR
+			out = x ^ y;
+			zero = (out==0)?1:0;
+			ng = (out<0)?1:0;
+			overflow = 0;
+		end else if (ALUFn[4:0] == 4'b0100) begin // SLL
+			out = x << y;
+			
+			zero = (out==0)?1:0;
+			ng = (out<0)?1:0;
+			overflow = 0;
+		end else if (ALUFn[4:0] == 4'b1000) begin // SRL
+			out = x >> y;
+			zero = (out==0)?1:0;
+			ng = (out<0)?1:0;
+			overflow = 0;
+		end
+	end
+endmodule
---
+++ Modules/ALU_Control.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+
+module alu_control(
+    input      [31:0] instruction,
+    input      [1:0]  ALUOp,
+    output reg [3:0]  ALUFn
+);
+
+always @(instruction or ALUOp)
+begin
+    if (ALUOp == 2'b00) begin //Load Word, Store Word, I type (ADD)
+        // ALUFn <= instruction[5:0];
+	if (instruction[6:5] == 2'b00) begin // Load word
+		ALUFn <= 5'b0010; //ADD
+	end else if (instruction[6:5] == 2'b01) begin // Store word
+		ALUFn <= 5'b0010; // ADD
+	end
+    end else if (ALUOp == 2'b10) begin // R Type
+	if (instruction[31:24] == 7'b0000000) begin
+		end else if (instruction[14:12] == 3'b000) begin // ADD
+			ALUFn <= 4'b0010; // ADD
+		end else if (instruction[14:12] == 3'b111) begin // AND
+			ALUFn <= 4'b0000;
+		end else if (instruction[14:12] == 3'b110) begin // OR
+			ALUFn <= 4'b0001; // OR
+		end else if (instruction[14:12] == 3'b100) begin // XOR
+			ALUFn <= 4'b0011; // XOR
+		end else if (instruction[14:12] == 3'b001) begin // SLL
+			ALUFn <= 4'b0100; // SLL
+		end else if (instruction[14:12] == 3'b101) begin // SRL
+			ALUFn <= 4'b1000; // SRL
+		end
+    end else if (instruction[31:24] == 7'b0100000) begin
+		if (instruction[14:12] == 3'b000) begin // SUB
+			ALUFn <= 4'b0110; // SUB
+		end
+    end else if (ALUOp == 2'b01) begin // SB Type
+       	ALUFn <= 4'b0110; // SUB
+   end
+
+end
+
+endmodule

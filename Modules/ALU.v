4e3689691d71b64ba2b1fdcd88ac47aa1b808cdf
V Dheeraj <41289602+dheerajrox@users.noreply.github.com>
2020-11-13 13:00:15 +0530

    Add files via upload

--- Modules/ALU.v
+++ Modules/ALU.v
@@ -1,10 +1,11 @@
+
 `timescale 1ns / 1ps
 
 module ALU(clk, x, y, out, ALUFn, zero, ng, overflow);
 	input wire clk;
 	input wire [31:0] x;
 	input wire [31:0] y;
-	input wire [4:0] ALUFn;
+	input wire [3:0] ALUFn;
 	output reg [31:0] out;
 	output reg zero; 
 	output reg ng; 
@@ -12,7 +13,7 @@ module ALU(clk, x, y, out, ALUFn, zero, ng, overflow);
 
 	always @(x or y or ALUFn)
 	begin
-		if (ALUFn[4:0] == 4'b0010) begin // ADD
+		if (ALUFn[3:0] == 4'b0010) begin // ADD
 			out = x + y;
 			zero = (out==0)?1:0;
 			ng = (out<0)?1:0;
@@ -21,7 +22,7 @@ module ALU(clk, x, y, out, ALUFn, zero, ng, overflow);
                 	else
                     		overflow = 0;
                 	
-            	end else if (ALUFn[4:0] == 4'b0110) begin // SUB
+            	end else if (ALUFn[3:0] == 4'b0110) begin // SUB
 			out = x - y;
 			zero = (out==0)?1:0;
 			ng = (out<0)?1:0;
@@ -30,30 +31,30 @@ module ALU(clk, x, y, out, ALUFn, zero, ng, overflow);
                 	else
                     		overflow = 0;
 			
-		end else if (ALUFn[4:0] == 4'b0000) begin // AND
+		end else if (ALUFn[3:0] == 4'b0000) begin // AND
 			out = x & y;
 			
 			zero = (out==0)?1:0;
 			ng = (out<0)?1:0;
 			overflow = 0;
-		end else if (ALUFn[4:0] == 4'b0001) begin // OR
+		end else if (ALUFn[3:0] == 4'b0001) begin // OR
 			out = x | y;
 			
 			zero = (out==0)?1:0;
 			ng = (out<0)?1:0;
 			overflow = 0;
-		end else if (ALUFn[4:0] == 4'b0011) begin // XOR
+		end else if (ALUFn[3:0] == 4'b0011) begin // XOR
 			out = x ^ y;
 			zero = (out==0)?1:0;
 			ng = (out<0)?1:0;
 			overflow = 0;
-		end else if (ALUFn[4:0] == 4'b0100) begin // SLL
+		end else if (ALUFn[3:0] == 4'b0100) begin // SLL
 			out = x << y;
 			
 			zero = (out==0)?1:0;
 			ng = (out<0)?1:0;
 			overflow = 0;
-		end else if (ALUFn[4:0] == 4'b1000) begin // SRL
+		end else if (ALUFn[3:0] == 4'b1000) begin // SRL
 			out = x >> y;
 			zero = (out==0)?1:0;
 			ng = (out<0)?1:0;
